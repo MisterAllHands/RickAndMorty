fddee281ac1f1407d295f503ca6c6fcbbcde3892
MisterAllHands <aldoknownas@gmail.com>
2023-08-21 11:58:51 +0300

    Bug with loading additonal characters

---
+++ RickandMorty/Views/FooterCollectionLoadingCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  FooterCollectionLoadingCollectionReusableView.swift
+//  RickandMorty
+//
+//  Created by TTGMOTSF on 20/08/2023.
+//
+
+import UIKit
+
+class FooterCollectionLoadingCollectionReusableView: UICollectionReusableView {
+        
+}
